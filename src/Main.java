f05ecb5c27bb1d45fc08ca50302574a3f9fd4ecf
abdul basith p v <basithofficialmail01@gmail.com>
2025-11-22 09:43:12 +0530

    basic simple logic of calculator

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Main {
 
-    public static int main(String[] args) {
+    public static void main(String[] args) {
             while(true)
             {
                 System.out.println("Please select an operation ");
-                System.out.println("1.Addition \n 2. Substraction \n 3. Division \n 4. Multiplication 5. Exit");
-                System.out.println("choose (1 -5 ) ");
+                System.out.print("1.Addition\n2.Substraction\n3.Division\n4.Multiplication\n5.Exit\n");
+                System.out.print("choose (1 -5 ): ");
                 Scanner sc=new Scanner(System.in);
                 int choice =sc.nextInt();
                 System.out.println("enter the first number :");
@@ -27,7 +27,7 @@ public class Main {
                     }break;
                     case 4: System.out.println(num1*num2);
                         break;
-                    case 5: return 0;
+                    case 5: return;
                     default:System.out.println("enter a valid option from 1 to 5");
                 }
             }
